086afa4a6f1fa95109e40a872d4b170c3eb1c488
paula tongson <paulamaeivana.tongson@benilde.edu.ph>
2021-07-23 23:08:47 +0700

    modify mssql

--- src/database/mssql.ts
+++ src/database/mssql.ts
@@ -14,7 +14,7 @@ const sqlConfig = {
   },
   options: {
     encrypt: true, // for azure
-    trustServerCertificate: false, // change to true for local dev / self-signed certs
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
   },
 };
 
